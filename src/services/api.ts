806f54281c05331f954c5b7c2eb828e6db89fd1a
jakesuche <uchechidi9@gmail.com>
2021-12-03 14:25:16 +0100

    adjusted mobile responsiveness

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import Axios from 'axios';
 import { JURIDOC_API } from '../configs/Environment';
-
+const API:string = 'https://juridoc-v3.herokuapp.com/api/v2'
 const api = Axios.create({
-  baseURL: JURIDOC_API,
+  baseURL: API,
   withCredentials: true,
 });
 
-
+// JURIDOC_API
 
 api.interceptors.response.use(
   (response) => response,
