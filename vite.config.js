74e73a0dd8b9602c4ceebfdf88690e363f091428
jakesuche <uchechidi9@gmail.com>
2021-12-03 20:26:31 +0100

    he

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base:'https://v3test.netlify.app/',
   plugins: [vue()],
   server: {
     port: 8080,
